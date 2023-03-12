73f93ed64d11dad22d9d5f0da40d40c5ad82f379
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-03-12 20:13:38 +0100

    define first colors && screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,23 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "af-white": "#FEF9FF",
+        "plat": "#DBD9DB",
+        "rose": "#B098A4",
+        "gray": "747572",
+        "ebony": "45503B"
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [],
 }
