ddbb8b0119dd563cd3810cff17403c33eead80bd
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-03-20 21:48:51 +0100

    `catalysts` basic done

--- pages/catalysts/index.tsx
+++ pages/catalysts/index.tsx
@@ -1,6 +1,26 @@
 const index = () => {
   return (
-    <section>Catalyst</section>
+    <section className="w-5/6 max-w-[1200px] mx-auto py-8">
+      <h3 className="text-3xl flex justify-center font-bold">Katalizatory</h3>
+      <p className="pt-6">
+        Oferujemy heterogeniczne katalizatory, użyteczne w syntezie organicznej   przy tworzeniu wiązań węgiel-węgiel
+        Katalizatory są aktywne w sub-stechiometrycznych ilościach (podać TON/TOF   dla znanych układów) i otrzymujemy je z doskonałą powtarzalnością.
+        W stałej ofercie dostępnych jest kilka wariantów.
+        Na zlecenie produkowane są inne układy katalityczne, dostępne w ilościach   miligram - kilogram.
+        Możliwa jest produkcja układów o zróżnicowanej i dedykowanej morfologii,  zawierających jeden metal lub kilka. Możemy także przygotować bibliotekę o   ustalonej konfiguracji - rodzaje metali, ich dystrybucja - i pośrednio  stężenie, rozmiar ziaren, rodzaj podłoża, rozkład dyspersji -  monodyspersyjność.
+        Katalizatory zawierają metale ciężkie, nieszlachetne, szlachetne i ziem   rzadkich.
+        Ze względu na badawczo-rozwojowy charakter naszego laboratorium, możemy   zapewnić charakterystykę materiałów jakie otrzymujemy. Przykładowo analizy  SEM, TEM, FTIR i XRD.
+      </p>
+
+      <p>
+        Oferujemy organokatalizatory, związki koordynacyjne, będące połączeniem   metali i ligandów organicznych. Związki te przygotowujemy w ścisłej   współpracy z klientami z uwagi na specyficzne wymagania co do budowy i  charakterystyki funkcjonalnej materiału.
+      </p>
+
+      <h6 className="py-6 font-bold">Technologie wodorowe</h6>
+      <p>
+        Nasze osiągnięcia w obszarze materiałów funkcjonalnych skupiają się także   w szeroko pojętej dziedzinie technologii wodorowych. Obecnie testujemy  katalizator dedykowany do krakingu amoniaku. Pomysł wykorzystania amoniaku   jako źródła wodoru ma długą historię. Niewątpliwie atrakcyjna jest idea   wykorzystania gazów powstających w wyniku rozkładu cząsteczki NH3, do   bezpośredniego zasilania ogniw paliwowych. Katalizator, który testujemy ma  dodatni bilans energetyczny ze względu na jego aktywność w temperaturach   poniżej 450 *C - jaki ten bilans w liczbach, i wykazuje ponad 99,9%  efektywność działania w trakcie wielodniowych testów laboratoryjnych.  Zaletą jest niski koszt wytworzenia oraz skalowalna technologia produkcji.   Więcej informacji udzielamy na życzenie. Przygotowujemy wniosek grantowy w  konkursie Europejskim.... na dalszy rozwój tego materiału, ponieważ  dostrzegamy możliwość dalszego obniżania temperatury efektywnego rozkładu  amoniaku. Planujemy także demonstrator dla branży automotive.
+      </p>
+    </section>
   )
 }
 
