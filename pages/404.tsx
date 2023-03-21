73eb290736a09039e574f0275984f5cdcb7611ce
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-03-21 20:04:47 +0100

    `404` add automatic redirect to home page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link"
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const NotFound = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/')
+    }, 3000)
+  }, [])
+
   return (
     <div className="w-full h-[calc(100vh-478px)] flex flex-col justify-center items-center">
       <h3>Oooops...</h3>
       <h4 className="py-6">Strona - której szukasz, nie istnieje.</h4>
-      <p>Powróć do <Link href="/" className="underline">Strony Głównej.</Link></p>
+      <p>Powróć do <Link href="/" className="underline text-blue-500">Strony Głównej.</Link></p>
     </div>
   )
 }
