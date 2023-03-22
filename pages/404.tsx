c96567f405a7d532e35c465ffa455db30e0d72ca
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-03-22 21:20:22 +0100

    `404` little redesign

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,6 @@
-import Link from "next/link"
 import { useEffect } from 'react'
+import Link from "next/link"
+import Image from "next/image"
 import { useRouter } from 'next/router'
 
 const NotFound = () => {
@@ -12,11 +13,18 @@ const NotFound = () => {
   }, [])
 
   return (
-    <div className="w-full h-[calc(100vh-478px)] flex flex-col justify-center items-center">
-      <h3>Oooops...</h3>
-      <h4 className="py-6">Strona - której szukasz, nie istnieje.</h4>
-      <p>Powróć do <Link href="/" className="underline text-blue-500">Strony Głównej.</Link></p>
-    </div>
+    <section className="w-full h-[calc(100vh-478px)] flex justify-center items-center gap-12">
+      <figure>
+        <Image src="/assets/monitor.png" alt="error-404-icon" width={300} height={400} />
+      </figure>
+
+      <div>
+        <h3 className="font-bold text-4xl">Oooops... Coś poszło nie tak.</h3>
+        <h4 className="text-2xl py-6">Strona - której szukasz, nie istnieje.</h4>
+        <p>Powróć do <Link href="/" className="underline text-blue-500">Strony Głównej.</Link></p>
+      </div>
+      
+    </section>
   )
 }
 
