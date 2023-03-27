32071d7b22e8d9246eaae0f7f637f334d1cd7ccd
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-03-27 20:15:54 +0200

    `Contact` basic done

--- pages/landing/Contact.tsx
+++ pages/landing/Contact.tsx
@@ -7,11 +7,44 @@ const Contact = () => {
           <p className="text-af-white text-xl w-3/4">Witam! Z niecierpliwością czekamy na kontakt od Ciebie.</p>
         </div>
 
-        <div className="md:flex md:flex-col w-1/2 gap-y-3">
-          <input type="text" className="h-[45px] px-4" placeholder="Wpisz imię i nazwisko." />
-          <input type="text" className="h-[45px] px-4" placeholder="Podaj adres email." />
-          <textarea rows={6} className="p-4" placeholder="Wpisz wiadomość..." />
-        </div>
+        <form 
+          className="md:flex md:flex-col items-end w-1/2 gap-y-3"
+        >
+          <input
+            id="name"
+            className="w-full h-[45px] px-4"
+            type="text"
+            placeholder="Wpisz imię i nazwisko."
+            required
+            minLength={5}
+            maxLength={30}
+          />
+          <input
+            id="email"
+            type="text"
+            className="w-full h-[45px] px-4"
+            placeholder="Podaj adres email."
+            required
+            minLength={6}
+            maxLength={30}
+            pattern=""
+            title="Email musi"
+          />
+          <textarea
+            id="message"
+            rows={6}
+            className="w-full p-4"
+            placeholder="Wpisz wiadomość..."
+            required
+            maxLength={200}
+          />
+          <button
+            type="submit"
+            className="h-[45px] cursor-pointer"
+          >
+            Wyślij wiadomość
+          </button>
+        </form>
       </div>
     </section>
   )
