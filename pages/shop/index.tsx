5b78b5000452dd26246cd9f58ebf09775eeaf527
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-03-27 20:15:41 +0200

    trying to form ok iframe

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import IframeResizer from "iframe-resizer-react"
 
 const index = () => {
   return (
@@ -8,7 +9,19 @@ const index = () => {
           Siltim Sp. z o.o. | Sklep
         </title>
       </Head>
-      <section>Shop</section>
+      <section className="w-5/6 max-w-[1200px] mx-auto">
+        <div className="w-full">
+          <IframeResizer 
+            src="https://siltim-chem-catalogue.onrender.com"
+            heightCalculationMethod="lowestElement"
+            minHeight={1546}
+            style={{
+              width: '1px',
+              minWidth: '100%',
+            }}
+          />
+        </div>
+      </section>
     </>
   )
 }
