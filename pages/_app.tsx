edac35e62000f2ed927fb2be5f541426f2890e2d
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-03-27 20:16:03 +0200

    added loadet to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
-import Layout from "../components/Layout"
+import { useState } from 'react'
+import Layout from '../components/Layout'
 import './global.css'
 import type { AppProps } from 'next/app'
 
+import Loader from './../components/loader'
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      {
+        isLoading ? <Loader /> : null
+      }
+    </>
   )
 }
