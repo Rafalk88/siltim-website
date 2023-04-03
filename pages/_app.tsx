0fdbb632fbb9ed8c5ef546eec0360b73cce9387b
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-04-03 22:28:36 +0200

    react slick installed and added to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import { ChakraProvider } from "@chakra-ui/react"
 
 import Layout from '../components/Layout'
-
-import './global.css'
-import type { AppProps } from 'next/app'
-
 import Loader from './../components/loader'
+
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import './global.css'
+
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [isLoading, setIsLoading] = useState<boolean>(false)
