6a7230fdf48280e54cdb5441245e9b532cc503ad
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-04-10 19:38:28 +0200

    `Navbar` reworked

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,20 +1,21 @@
-import { useState } from "react"
 import Image from 'next/image'
 import Link from "next/link"
+import { useState } from "react"
+import { Button } from "@chakra-ui/react"
+
+import { useMediaQuery } from "../../hooks/useMediaQuery"
+
+import Logo from '../../public/assets/Logo.png'
 import { RxHamburgerMenu } from "react-icons/rx"
 import { XMarkIcon } from "@heroicons/react/24/outline"
 
-import { useMediaQuery } from "../../hooks/useMediaQuery"
-import Logo from '../../public/assets/Logo.png'
-
 export const menu = [
-  {text: 'Główna', route: ''},
-  {text: 'O nas', route: 'about'},
-  {text: 'Syntezy na zlecenie', route: 'synthesis'},
-  {text: 'Katalizatory', route: 'catalysts'},
-  {text: 'Sklep', route: 'shop'},
+  {text: 'Główna', route: '', lineWidth: 100},
+  {text: 'O nas', route: 'about', lineWidth: 100},
+  {text: 'Katalizatory', route: 'catalysts', lineWidth: 100},
+  {text: 'Syntezy na zlecenie', route: 'synthesis', lineWidth: 195},
 ];
-
+ 
 const flexBetween = "flex items-center justify-between";
 
 type Props = {
@@ -29,32 +30,49 @@ export const index = ({
   const navbarBackground = isTopOfPage ? "" : "bg-gray drop-shadow bg-white"
 
   return (
-    <nav className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full
+    <nav className={`${navbarBackground} ${flexBetween} fixed top-[40px] z-30 w-full
     py-3`}
     >
-      <div className={`${flexBetween} mx-auto w-5/6 max-w-[1200px]`}>
-        <div className={`${flexBetween}`}>
-          <figure className="p-2 h-16 w-16">
+      <div className={`flex items-center mx-auto w-5/6 max-w-[1200px]`}>
+        <div className={`${flexBetween} w-1/6`}>
+          <div className="p-2 h-16 w-16">
             <Link href="/" className="cursor-pointer">
               <Image src={Logo} alt={"Logo"} />
             </Link>
-          </figure>
+          </div>
         </div>
 
         {
           isAboveMediumScreens ? (
-            <div>
-              <ul className={`${flexBetween} gap-8 text-sm text-rich-black`}>
+            <div className={`${flexBetween} w-5/6`}>
+              <ul className={`${flexBetween} gap-8 text-sm`}>
                 {
                   menu.map(item => {
                     return (
-                      <Link key={item.text} href={`/${item.route}`} className="cursor-pointer">
-                        {item.text}
-                      </Link>
+                      <div className={`flex flex-col before:content-[''] before:w-[${item.lineWidth}px]
+                        before:h-[1px] before:bg-black before:hover:bg-blue hover:text-blue`}
+                      >
+                        <Link key={item.text} href={`/${item.route}`} className="cursor-pointer text-sm">
+                          {item.text}
+                        </Link>
+                      </div>
+                      
                     )
                   })
                 }
               </ul>
+
+              <Link href={"/shop"} passHref>
+                <Button
+                  className="w-[125px] h-[42px]"
+                  as="a"
+                  variant="primary"
+                >
+                  Sklep
+                </Button>
+              </Link>
+
+              {/* SEARCH BTN HERE */}
             </div>
           ) : (
             <div>
