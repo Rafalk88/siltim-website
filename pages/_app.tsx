d05f886ceb2173267ad216674cb75869e99aa625
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-04-10 17:57:57 +0200

    chakra set theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import { ChakraProvider } from "@chakra-ui/react"
+import { extendTheme } from '@chakra-ui/react'
 
 import Layout from '../components/Layout'
-import Loader from './../components/loader'
+import Loader from '../components/Loader'
 
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
@@ -10,12 +11,19 @@ import './global.css'
 
 import type { AppProps } from 'next/app'
 
+const theme = extendTheme({
+  fonts: {
+    heading: `'Montserrat', sans-serif`,
+    body: `'Montserrat', sans-serif`
+  }
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
   return (
     <>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
