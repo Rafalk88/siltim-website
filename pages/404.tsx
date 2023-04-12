6b92b530b5936e42d42514702bb9d8fc9bae7ac3
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-04-12 23:38:16 +0200

    `404` added `Typography`

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react'
 import Link from "next/link"
 import Image from "next/image"
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router"
+
+import Typography from "@/components/Typography"
 
 const NotFound = () => {
   const router = useRouter()
@@ -13,15 +15,38 @@ const NotFound = () => {
   }, [])
 
   return (
-    <section className="w-full h-[calc(100vh-478px)] flex justify-center items-center gap-12">
+    <section className="w-full h-[calc(100vh-120px)] flex justify-center
+      items-center gap-12"
+    >
       <figure>
-        <Image src="/assets/monitor.png" alt="error-404-icon" width={300} height={400} />
+        <Image 
+          src="/assets/monitor.png"
+          alt="Error-404-icon"
+          width={300}
+          height={400}
+        />
       </figure>
 
       <div>
-        <h3 className="font-bold text-4xl">Oooops... Coś poszło nie tak.</h3>
-        <h4 className="text-2xl py-6">Strona - której szukasz, nie istnieje.</h4>
-        <p>Powróć do <Link href="/" className="underline text-blue-500">Strony Głównej.</Link></p>
+        <Typography
+          variant="h2"
+          as="h2"
+        >
+          Oooops... Coś poszło nie tak
+        </Typography>
+        <Typography
+          className="py-6 font-regular"
+          variant="h4"
+          as="h4"
+        >
+          Strona - której szukasz, nie istnieje
+        </Typography>
+        <Typography
+          variant="body"
+          as="p"
+        >
+          Powróć do <Link href="/" className="underline text-blue-500">Strony Głównej</Link>
+        </Typography>
       </div>
       
     </section>
