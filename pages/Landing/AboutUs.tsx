a87f33250f0764cd9b33ebce9135523ac460d793
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-04-12 18:13:18 +0200

    `AboutUs` reworked

--- pages/Landing/AboutUs.tsx
+++ pages/Landing/AboutUs.tsx
@@ -1,34 +1,47 @@
 import { Button } from "@chakra-ui/react"
-import { BookOpenIcon } from "@heroicons/react/24/solid"
+
+import ChemSlider from "./Slider"
+
+const paragraphStyles = "text-lg text-dark-grey"
 
 const AboutUs = () => {
   return (
-    <section className="w-5/6 max-w-[1200px] mx-auto md:flex">
-      <div className="md:w-4/6">
-        <h3 className="text-xl py-8 font-poppins font-bold">
-          Siltim to spółka o charakterze badawczo-rozwojowym.
+    <section className="w-5/6 max-w-[1200px] md:h-[calc(100vh-128px)] mx-auto md:flex
+      md:justify-center md:items-center md:gap-x-40"
+    >
+      <div className="w-1/2 pb-12 md:pb-0 md:w-1/3">
+        <ChemSlider 
+          className=""
+        />
+      </div> 
+
+      <div className="md:w-1/3">
+        <h3 className="text-5xl font-extrabold text-dark-grey">
+          Charakter badawczo-rozwojowy
         </h3>
-        <p className="text-md font-raleway">
-          Równolegle do działalności komercyjnej, pracujemy nad nowymi
-          technologiami. Zapraszamy do współpracy w zakresie współpracy
-          gospodarczej inne podmioty oraz inwestorów. Nasz zespół łączy
-          interdyscyplinarne kompetencje z zakresu chemii oraz inzynierii.
-          Mamy doświadczenie w przygotowywaniu wniosków konkursowych.
+        <p className={`${paragraphStyles} py-8`}>
+          Dzięki naszemu zaangażowaniu w projekty badawcze jakie inicjujemy, bądź w jakich partycypujemy,
+          chcielibyśmy wykazać wkład <br /> w postęp naukowy w dziedzinie chemii. Wierzymy, że odpowiedni
+          stopień automatyzacji oraz cyfryzacji tych procesów, otworzy nowe możliwości na drodze do nowych
+          struktur <br /> i przyczyni się do odkrycia nieznanych dotąd obszarów przestrzeni chemicznej. Z tego
+          względu testujemy i doskonalimy oryginalne rozwiązania od początku działalności.
         </p>
-      </div>
-      
-      <div className="py-8 md:py-0 md:w-2/6 md:flex md:flex-col md:justify-center
-        md:items-center"
-      >
+
+        <p className={`${paragraphStyles} pb-8`}>
+          Kluczowy personel badawczy w osobach dr Piotra Bartczaka oraz dr inż. Jacka Mularskiego
+          obejmuje kierownictwo oraz stawia nowe wyzwania w zakresie prac projektowych, syntez
+          chemicznych, strukturyzacji bazy danych <br /> i wdrożeniem chmury obliczeniowej opartej <br />
+          o metody uczenia maszynowego, i syntezy.
+        </p>
+
         <Button 
         className="w-[250px] h-[45px]
           cursor-pointer font-rajdhani font-bold"
         data-role="information"
-        leftIcon={<BookOpenIcon width={20} height={20}/>}
+        variant="primary"
         >
-          Badania i rozwój
+          Zobacz nasze osiągnęcia
         </Button>
-        <p className="py-2">Zapoznaj się z naszymi osiągnięciami</p>
       </div>
     </section>
   )
