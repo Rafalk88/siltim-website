ab46313a8a9c1f5ae50c53ef8428280b7f54ab01
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-04-13 19:31:23 +0200

    `theme` fixed displaying font

--- pages/theme.ts
+++ pages/theme.ts
@@ -1,9 +1,13 @@
 import { extendTheme } from '@chakra-ui/react'
 
+import '@fontsource/montserrat/400.css'
+import '@fontsource/montserrat/700.css'
+import '@fontsource/montserrat/800.css'
+
 export const theme = extendTheme({
   fonts: {
-    heading: `'Montserrat', sans-serif`,
-    body: `'Montserrat', sans-serif`
+    heading: `'montserrat', sans-serif`,
+    body: `'montserrat', sans-serif`
   },
   components: {
     Button: {
