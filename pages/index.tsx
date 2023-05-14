73926387f980c771df77cc9930e1e9a495523f97
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-05-14 14:47:40 +0200

    `Landing` deleted unnecessary prop

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,14 @@ import Head from 'next/head';
 import React from 'react';
 import Landing from './landing';
 
-type Props = {
-  isTopOfPage: boolean
-}
-
-const Home = ({ isTopOfPage }:Props) => {
+const Home = () => {
   
   return (
     <>
       <Head>
         <title>Siltim Sp. z o.o. | Strona główna</title>
       </Head>
-      <Landing isTopOfPage={isTopOfPage} />
+      <Landing />
     </>
   )
 };
