6041907df6b7e50186ea8a79e68f138fe99bce30
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-06-05 21:06:20 +0200

    `Anchor` changing mechanic to button with visible state

--- components/Anchor/index.tsx
+++ components/Anchor/index.tsx
@@ -1,23 +1,38 @@
+import { useState } from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
+import Contact from '@/pages/landing/Contact'
 
 import SecuredLetter from "@/public/assets/SecuredLetter.svg"
 
 const Anchor = () => {
+  const [isVisible, setIsVisible] = useState(false)
+
   return (
-    <section className="w-[80px] h-[80px] rounded-full flex justify-center
-      items-center bg-blue fixed top-[85%] right-[1%] cursor-pointer
-      drop-shadow-md hover:bg-blue/80 z-[1]"
-    >
-      <Link href="/#contact" className="cursor-pointer" passHref>
-        <Image
-          src={SecuredLetter}
-          alt={"Contact-icon"}
-          width={46}
-          height={46}
-        />
-      </Link>
-    </section>
+    <>
+      <section className="w-[80px] h-[80px] rounded-full flex justify-center
+        items-center bg-blue fixed top-[85%] right-[1%] cursor-pointer
+        drop-shadow-md hover:bg-blue/80 z-[1]"
+      >
+        <button 
+          className="cursor-pointer"
+          onClick={() => setIsVisible(prev => !prev)}
+        >
+          <Image
+            src={SecuredLetter}
+            alt={"Contact-icon"}
+            width={46}
+            height={46}
+          />
+        </button>
+      </section>
+      {
+        isVisible ? (
+          <div className="bg-white">
+            banan
+          </div>
+        ) : null
+      }
+    </>
   )
 }
 
