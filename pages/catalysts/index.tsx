9116bd30726c96a12a4760e7e7934b68492c2128
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-09-10 09:39:43 +0200

    CI(Catalysts): add more imgs

--- pages/catalysts/index.tsx
+++ pages/catalysts/index.tsx
@@ -6,6 +6,9 @@ import Typography from "@/components/Typography"
 import cataOnePhoto from "@/public/assets/cata1.png"
 import cataTwoPhoto from "@/public/assets/cata2.png"
 import cataThreePhoto from "@/public/assets/cata3.png"
+import cataFourPhoto from "@/public/assets/cata4.png"
+import cataFivePhoto from "@/public/assets/cata5.png"
+import cataSixPhoto from "@/public/assets/cata6.png"
 
 const index = () => {
   return (
@@ -18,7 +21,7 @@ const index = () => {
       <section className="w-5/6 max-w-[1200px] md:min-h-[calc(100vh-80px)] mx-auto
         flex flex-col justify-center items-center pt-40 pb-12"
       >
-        <div>
+        <>
           <Typography
             className="flex justify-center font-extrabold text-dark-grey pb-10"
             variant="h2"
@@ -68,20 +71,21 @@ const index = () => {
             </div>
             <div className="sm:w-1/2 sm:flex sm:flex-col sm:items-center">
               <Image
-                className="mb-10"
+                className="mb-10 rounded-xl"
                 src={cataOnePhoto}
                 alt="section photo"
                 width={300}
                 height={300}
               />
               <Image
-                className="mb-10"
+                className="mb-10 rounded-xl"
                 src={cataTwoPhoto}
                 alt="section photo"
                 width={300}
                 height={300}
               />
               <Image
+                className="rounded-xl"
                 src={cataThreePhoto}
                 alt="section photo"
                 width={300}
@@ -89,58 +93,82 @@ const index = () => {
               />
             </div>
           </div>
+        </>
+        <Typography
+          className="py-8 flex justify-center font-extrabold text-dark-grey"
+          variant="h2"
+          as="h2"
+        >
+          Technologie wodorowe
+        </Typography>
+        <div className="sm:flex sm:items-center">
+          <div className="sm:w-1/2 sm:flex sm:flex-col sm:items-center">
+            <Image
+              className="mb-10 rounded-xl"
+              src={cataFourPhoto}
+              alt="section photo"
+              width={300}
+              height={300}
+            />
+            <Image
+              className="mb-10 rounded-xl"
+              src={cataFivePhoto}
+              alt="section photo"
+              width={300}
+              height={300}
+            />
+            <Image
+              className="rounded-xl"
+              src={cataSixPhoto}
+              alt="section photo"
+              width={300}
+              height={300}
+            />
+          </div>
+          <div className="sm:w-1/2">
+            <Typography
+              variant="body"
+              as="p"
+            >
+              Bezpieczna i efektywna technologia umożliwiająca wykorzystanie wodoru jako
+              paliwa stanowi złożone wyzwanie technologiczne. Jedną z możliwości jest
+              wykorzystanie amoniaku jako nośnika (magazynu) wodoru. Głównym powodem dla
+              którego ta idea budzi zainteresowanie na skalę globalną, jest możliwość transportu
+              ciekłego amoniaku. Pod względem stechiometrycznym udział atomów wodoru jest
+              znacznie większy niż w przypadku innych nośników. Skroplony gaz zapewnia także
+              dużą, nieosiągalną dla innych rozwiązań, masę wodoru w jednostce objętości.
+              Obecnie prowadzimy III - fazę testów katalizatora, wykazującego następujące
+              właściwości:
+            </Typography>
+            <Typography
+              className="py-6"
+              variant="body"
+              as="ul"
+            >
+              <li>
+                - dodatni bilans energetyczny ze względu na jego aktywność w temperaturach
+                poniżej 450 *C (jaki ten bilans w liczbach)
+              </li>
+              <li>
+                - ponad 99,9% efektywność działania w trakcie wielodniowych testów
+                laboratoryjnych.
+              </li>
+              <li>
+                - niski koszt wytworzenia oraz skalowalna technologia produkcji.
+              </li>
+            </Typography>
+            <Typography
+              className="py-6"
+              variant="body"
+              as="p"
+            >
+              Poszukujemy podmiotów zainteresowanych współpracą ukierunkowaną na
+              wdrożenie rynkowe katalizatora oraz badaniami nad dalszą poprawą efektywności
+              procesu rozkładu amoniaku (obniżaniem temperatury procesu - poprawą bilansu
+              energetycznego). Mile widziane projekty z branży automotive.
+            </Typography>
+          </div>
         </div>
-        <div>
-          <Typography
-            className="py-8 flex justify-center font-extrabold text-dark-grey"
-            variant="h2"
-            as="h2"
-          >
-            Technologie wodorowe
-          </Typography>
-          <Typography
-            variant="body"
-            as="p"
-          >
-            Bezpieczna i efektywna technologia umożliwiająca wykorzystanie wodoru jako
-            paliwa stanowi złożone wyzwanie technologiczne. Jedną z możliwości jest
-            wykorzystanie amoniaku jako nośnika (magazynu) wodoru. Głównym powodem dla
-            którego ta idea budzi zainteresowanie na skalę globalną, jest możliwość transportu
-            ciekłego amoniaku. Pod względem stechiometrycznym udział atomów wodoru jest
-            znacznie większy niż w przypadku innych nośników. Skroplony gaz zapewnia także
-            dużą, nieosiągalną dla innych rozwiązań, masę wodoru w jednostce objętości.
-            Obecnie prowadzimy III - fazę testów katalizatora, wykazującego następujące
-            właściwości:
-          </Typography>
-          <Typography
-            className="py-6"
-            variant="body"
-            as="ul"
-          >
-            <li>
-              - dodatni bilans energetyczny ze względu na jego aktywność w temperaturach
-              poniżej 450 *C (jaki ten bilans w liczbach)
-            </li>
-            <li>
-              - ponad 99,9% efektywność działania w trakcie wielodniowych testów
-              laboratoryjnych.
-            </li>
-            <li>
-              - niski koszt wytworzenia oraz skalowalna technologia produkcji.
-            </li>
-          </Typography>
-          <Typography
-            className="py-6"
-            variant="body"
-            as="p"
-          >
-            Poszukujemy podmiotów zainteresowanych współpracą ukierunkowaną na
-            wdrożenie rynkowe katalizatora oraz badaniami nad dalszą poprawą efektywności
-            procesu rozkładu amoniaku (obniżaniem temperatury procesu - poprawą bilansu
-            energetycznego). Mile widziane projekty z branży automotive.
-          </Typography>
-        </div>
-        
       </section>
     </>
     
