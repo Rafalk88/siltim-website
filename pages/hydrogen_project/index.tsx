0cb7445d9748f308003539f30e6a1a8ab1f0386a
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-11-14 20:19:17 +0100

    feat(hydrogenProject): add page

---
+++ pages/hydrogen_project/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from "next/head"
+
+const index = () => {
+  return (
+    <>
+      <Head>
+        <title>
+          Siltim Sp. z o.o. | Projekt wodorowy
+        </title>
+      </Head>
+      <section className="w-5/6 max-w-[1200px] md:min-h-[calc(100vh-80px)] mx-auto pt-40 pb-12">index</section>
+    </>
+  )
+}
+
+export default index
