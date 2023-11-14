b5829c48efc9cfef25afcd74df3e3a4b0b5ade62
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-11-14 20:09:42 +0100

    chore: change to static export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
   images: {
+    unoptimized: true,
+    loader: 'custom',
+    loaderFile: './my-loader.ts',
     remotePatterns: [
       {
         protocol: 'https',
